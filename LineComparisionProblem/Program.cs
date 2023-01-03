06cdf943c72e6ee59263c0659bf9d5bbc1b56d83
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-03 10:09:44 +0530

    Check Equality of Two Lines

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -4,24 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Line Comparison");
+            Console.WriteLine("Line Comparison Problem");
+            Console.WriteLine("\n===========Line1=======\n");
 
             int x1, y1, x2, y2;
 
-            Console.WriteLine("\nEnter x1 co-oridinate: ");
+            Console.WriteLine("\nEnter x1 co-ordinate: ");
             x1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter y1 co-oridinate: ");
+            Console.WriteLine("Enter y1 co-ordinate: ");
             y1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\nEnter x2 co-oridinate: ");
+            Console.WriteLine("\nEnter x2 co-ordinate: ");
             x2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter y2 co-oridinate: ");
+            Console.WriteLine("Enter y2 co-ordinate: ");
             y2 = Convert.ToInt32(Console.ReadLine());
 
-            double lengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine($"\n length of the line is {lengthOfLine}");
+            double lengthOfLine1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"\n length of the line is {lengthOfLine1}");
+
+
+            Console.WriteLine("\n===========Line 2======\n");
+            int a1, b1, a2, b2;
+            Console.WriteLine("Enter a1 co-ordinate: ");
+            a1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter b1 co-ordinate: "  );
+            b1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a2 co-ordinate: ");
+            a2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter b2 co-ordinate: " );
+            b2 = Convert.ToInt32(Console.ReadLine());
+
+            double lengthofLine2 = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+            Console.WriteLine($"\nLength of the Line is {lengthofLine2}");
+
+            if (int.Equals(lengthOfLine1, lengthofLine2)) 
+            {
+                Console.WriteLine("The two lines are Equals");
+            }
+            else
+            {
+                Console.WriteLine("The two lines are not equal");
+            }
+
 
         }
     }
